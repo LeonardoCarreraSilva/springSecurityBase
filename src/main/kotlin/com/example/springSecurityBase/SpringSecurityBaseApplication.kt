bb5d4b70e929670f135e58722cda7882ac50fe4a
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2024-11-19 16:29:50 -0300

    commit inicial

---
+++ src/main/kotlin/com/example/springSecurityBase/SpringSecurityBaseApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springSecurityBase
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringSecurityBaseApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringSecurityBaseApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springSecurityBase/SpringSecurityBaseApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springSecurityBase
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringSecurityBaseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
